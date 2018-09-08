cc9cbc91443312629b8cfd524d30ebcca89e823d
guixiv <1072869448@qq.com>
2018-09-08 19:30:07 +0800

    Initial Commit

---
+++ 词频统计/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  词频统计
+//
+//  Created by wenyiqian on 2018/9/8.
+//  Copyright © 2018年 ding. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
